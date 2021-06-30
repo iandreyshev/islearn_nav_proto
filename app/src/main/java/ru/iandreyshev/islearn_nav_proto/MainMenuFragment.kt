a657f419c38cd828c23b05131564d5bf97185a48
Ivan Andreyshev <ivan.andreyshev@gmail.com>
2021-06-30 23:44:58 +0300

    first commit

---
+++ app/src/main/java/ru/iandreyshev/islearn_nav_proto/MainMenuFragment.kt
@@ -0,0 +1,5 @@
+package ru.iandreyshev.islearn_nav_proto
+
+import androidx.fragment.app.Fragment
+
+class MainMenuFragment : Fragment(R.layout.fragment_main_menu)
---
+++ app/src/main/java/ru/iandreyshev/islearn_nav_proto/UrlFragment.kt
@@ -0,0 +1,5 @@
+package ru.iandreyshev.islearn_nav_proto
+
+import androidx.fragment.app.Fragment
+
+class UrlFragment : Fragment(R.layout.fragment_url)
